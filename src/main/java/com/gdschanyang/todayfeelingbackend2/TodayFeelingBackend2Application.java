9c176b166d63145f59a4aa2a60b38e1853c73b5f
grand7070 <grand7070@naver.com>
2023-01-13 01:48:42 +0900

    initial setting

---
+++ src/main/java/com/gdschanyang/todayfeelingbackend2/TodayFeelingBackend2Application.java
@@ -0,0 +1,13 @@
+package com.gdschanyang.todayfeelingbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayFeelingBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayFeelingBackend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdschanyang/todayfeelingbackend2/TodayFeelingBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdschanyang.todayfeelingbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayFeelingBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
